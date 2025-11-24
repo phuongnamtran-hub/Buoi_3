86aa72e3b6b2aa9f8f92c5bca73a8ccdd606866b
TranPhuongNam <phuongnamtran054@gmail.com>
2025-11-24 17:02:47 +0700

    first commit

---
+++ Bai 1.py
@@ -0,0 +1,100 @@
+# while True:
+#     try:
+#         n = int(input("Nhap n: "))
+#         if(n>0):
+#             break
+#     except ValueError:
+#         print("Loi nhap sai kieu du lieu.")
+# Cau 1
+# print("====Cau 1====")
+# for i in range(1, n+1):
+#     if i%2==0:
+#         print(i,end=" ")
+
+# print()
+
+# # Cau 2
+# print("====Cau 2====")
+# tong = 0
+# for i in range(1, n+1):
+#     if(i%2 != 0):
+#         tong += i
+# print(f"Tong so le tu 1 den {n} la: {tong}")
+
+print()
+
+# Cau 3
+# print("====Cau 3====")
+
+print()
+
+# Cau 4
+# print("====Cau 4====") 
+# i = 1
+# dem = 0
+# while (i<=10):
+#     try:
+#         so = int(input(f"Nhap so thu {i}: "))
+#         i += 1
+#         if(so<0):
+#             dem += 1
+#     except ValueError:
+#         print("Loi nhap sai kieu du lieu.")
+
+# print(f"Co {dem} so am!")
+
+print()
+
+# Cau 5
+# print("====Cau 5====")
+
+print()
+
+# Cau 6
+# print("====Cau 6====")
+
+
+print()
+
+# Cau 7
+# print("====Cau 7====")
+
+print()
+
+# Cau 8
+# print("====Cau 8====")
+# while True:
+#     try:
+#         n = int(input("Nhap n: "))
+#         if(n>0):
+#             break
+#     except ValueError:
+#         print("Loi nhap sai kieu du lieu.")
+
+# i = 1
+# max = -1000
+# min = 1000
+# while (i<n):
+#     try:
+#         so = int(input(f"Nhap so thu {i}: "))
+#         i+=1
+#         if(so>max):
+#             max = so
+#         if(so<min):
+#             min = so
+#     except ValueError:
+#         print("Sai kieu du lieu.")
+# print(f"Gia tri lon nhat la {max}, nho nhat la {min}")
+
+print()
+
+# Cau 9
+# so = 12345
+# SO = so
+# dem = 0
+# while(so!=0):
+#     so = so//10
+#     dem += 1
+# print(f"So {SO} co {dem} chu so!")
+
+# Cau 10
